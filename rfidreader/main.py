02ca883309634d6a60dd0ac1f7a08b1922bb9e1d
Fjodor van Slooten <f.vanslooten@utwente.nl>
2025-09-28 08:54:04 +0200

    Update main.py
    Added more comments in main.py

--- rfidreader/main.py
+++ rfidreader/main.py
@@ -1,8 +1,11 @@
+# basic setup of RFID reader (begin with this):
+# https://toptechboy.com/connecting-and-using-the-rfid-rc522-rfid-tag-in-a-raspberry-pi-pico-project/
+# lock/unlock example:
 # https://toptechboy.com/rfid-lock-and-unlock-demonstration-project-raspberry-pi-pico-w/
 # Video's:
 # https://www.youtube.com/watch?v=b2Kgk16mtTY <- watch this first!
 # https://www.youtube.com/watch?v=bM-mr9X-SiU
-# library:
+# library (in folder libs\mfrc522):
 # https://github.com/sunfounder/kepler-kit
 
 from mfrc522 import SimpleMFRC522
