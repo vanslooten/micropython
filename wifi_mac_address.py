be20954a95c9476258abeca74766ffc38d775b86
Fjodor van Slooten <f.vanslooten@utwente.nl>
2026-04-10 10:13:41 +0200

    Add tutorial references to wifi_mac_address.py
    Added tutorial references for MAC address example.

--- wifi_mac_address.py
+++ wifi_mac_address.py
@@ -1,6 +1,9 @@
 # Example of how to get MAC address of Raspberry Pi Pico W
 # References:
 # https://forums.raspberrypi.com/viewtopic.php?t=346400
+#
+# this example is used in this tutorial:
+# https://home.et.utwente.nl/slootenvanf/2025/09/03/connect-to-wi-fi-raspberry-pi-pico-w/
 
 import network
 import ubinascii
